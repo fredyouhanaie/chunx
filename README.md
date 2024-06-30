87977b1d2dcad50b4695285802770423190e8dfb
Fred Youhanaie <fyrlang@anydata.co.uk>
2024-06-30 18:49:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# chunx
+
+An OTP library for handling Erlang EEP-48 document chunks.
+
+The initial purpose of this library is to enable the creation of man
+pages, which are no longer provided with Erlang/OTP 27.0.
+
+## Build
+
+    $ rebar3 compile
---
+++ src/chunx.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author Fred Youhanaie <fyrlang@anydata.co.uk>
+%%% @copyright 2024, Fred Youhanaie
+%%% @doc
+%%%
+%%% Utility functions for handling EEP-8 document chunks.
+%%%
+%%% @end
+%%% Created : 2024-06-30 by Fred Youhanaie <fyrlang@anydata.co.uk>
+%%%-------------------------------------------------------------------
+-module(chunx).
+
+-export([]).
